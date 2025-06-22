8deaa7312ec4ec8469ac5d40ffc89caf52cf98c2
joshua-dyer <jdyer.jsg@gmail.com>
2025-06-22 10:53:03 -0500

    Adding a view selector and adjusting the initial path to image folder for testing.

--- script.js
+++ script.js
@@ -6,13 +6,21 @@ const captureButton = document.getElementById("capture-button");
 const currentlyDisplayedImage = document.getElementById("base-diagram-image");
 const labelsElement = document.getElementById("labels-overlay-image");
 
+let pathToImages = "plax";
+
 // Set the initial visibility of labels
 labelsElement.style.display = 'none';
 
 
+
+
+// functions to change between Diagram, Labels, and Capture
+// These will be called when the cooresponding buttons are pressed
+// on the main page.  
+
 function switchToDiagram(){
     
-    currentlyDisplayedImage.src = "./images/echo/plax/diagram.png"
+    currentlyDisplayedImage.src = `./images/echo/${pathToImages}/diagram.png`;
 }
 
 function switchToLabels(){
@@ -27,5 +35,5 @@ function switchToLabels(){
 
 function switchToCapture(){
 
-    currentlyDisplayedImage.src = "./images/echo/plax/capture.png"
+    currentlyDisplayedImage.src = `./images/echo/${pathToImages}/capture.png`;
 }
