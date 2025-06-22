1887dd7a79d9ce0a5e22ab08ac50285f323f5831
joshua-dyer <jdyer.jsg@gmail.com>
2025-06-22 13:08:48 -0500

    Added all images for additional views

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ const captureButton = document.getElementById("capture-button");
 const currentlyDisplayedImage = document.getElementById("base-diagram-image");
 const labelsElement = document.getElementById("labels-overlay-image");
 
+// Set the initial path to images to the PLAX view
 let pathToImages = "plax";
 
-// Set the initial visibility of labels
-labelsElement.style.display = 'none';
+
 
 
 
@@ -23,17 +23,31 @@ function switchToDiagram(){
     currentlyDisplayedImage.src = `./images/echo/${pathToImages}/diagram.png`;
 }
 
-function switchToLabels(){
+function toggleLabels(){
 
-    if(labelsElement.style.display === 'none' || labelsElement.style.display === ""){
-        labelsElement.style.display = 'block';
-    } else{
-        labelsElement.style.display = 'none';
-    }
+    // Toggle the 'is-visible' class
+    labelsElement.classList.toggle('is-visible');
     
 }
 
 function switchToCapture(){
 
     currentlyDisplayedImage.src = `./images/echo/${pathToImages}/capture.png`;
+}
+
+
+//  Function to adjust path to images when Select element changes
+//  This will also set the Diagram view with no labels by default 
+//  in order to reset the view.
+
+function changePathToImages(){
+    // set the new path
+    pathToImages = document.getElementById("view-selector").value;
+    // hide the labels
+
+    // set new label path
+    labelsElement.src = `./images/echo/${pathToImages}/labels.png`;
+    // switch to diagram view
+    switchToDiagram();
+
 }
